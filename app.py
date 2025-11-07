81a52ed34a2eb0ce89e93fedfd10f1b499849f4a
Ayush Akolkar <ayush@example.com>
2025-11-07 09:06:18 +0530

    Fix vercel serverless configuration

--- app.py
+++ app.py
@@ -214,6 +214,6 @@ def about():
 def health():
     return "OK", 200
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+app = Flask(__name__)
+
+
