e0f75a5040d4a8324e1373b1516e1572dac87e74
Ayush Akolkar <ayush@example.com>
2025-11-07 09:26:31 +0530

    Added home route

--- app.py
+++ app.py
@@ -214,6 +214,11 @@ def about():
 def health():
     return "OK", 200
 
-app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('login.html')
 
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
